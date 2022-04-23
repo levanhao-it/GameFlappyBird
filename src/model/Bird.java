6ec17605c1d8e644fc277b9fb7fea96bf4f38f85
haole1411 <haole7301@gmail.com>
2022-04-23 14:07:43 +0700

    create project

---
+++ src/model/Bird.java
@@ -0,0 +1,13 @@
+package model;
+
+public abstract class Bird {
+	protected int x, y;
+	protected Animation animation;
+	public static final int BOUNCE = -7;
+	public static final int FALL = 3;
+
+	public static void main(String[] args) {
+
+	}
+
+}
