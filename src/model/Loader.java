a5a4be9e9dea7fb64ddb139f9474ad6921d33a0e
Pham Dinh Hoc <92513591+pdlearnit@user.noreply.github.com>
2022-05-17 20:59:50 +0700

    Commit of Hoc
    Commit model Column, FrameView,  Loader

---
+++ src/model/Loader.java
@@ -0,0 +1,24 @@
+package model;
+/**
+ * Cần một đối tượng làm nhiệm vụ tải ảnh lên từ src ảnh;
+ */
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+/**
+ * Object: Loader;
+ * @author phamdinhhoc
+ *
+ */
+public class Loader {
+	private static  Loader loader=new Loader() ;
+	/**
+	 * Tải ảnh lên từ src ảnh;
+	 * @param filename
+	 * @return
+	 */
+	public static Image loadImage(String filename) {
+		return new ImageIcon(loader.getClass().getResource("/images/" + filename)).getImage();
+	}
+	
+}
