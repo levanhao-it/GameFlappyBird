cb2876998acc04263f910725fcb2b0f78683afec
haole1411 <haole7301@gmail.com>
2022-05-24 21:53:50 +0700

    up

--- src/model/StateStart.java
+++ src/model/StateStart.java
@@ -5,10 +5,7 @@ public class StateStart implements StateGame {
 	@Override
 	public void update(Model game) {
 		game.getGround1().update();
-		// game.getSky1().update();
 		game.getGround2().update();
-		// game.getSky2().update();
-
 		game.getBird().update(1);
 		game.getSky().update();
 
