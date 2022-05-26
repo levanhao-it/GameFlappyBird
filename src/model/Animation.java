cbf51df9fc72d872e0cfe4c847a27eee9c7f9012
haole1411 <haole7301@gmail.com>
2022-05-26 18:27:49 +0700

    up

--- src/model/Animation.java
+++ src/model/Animation.java
@@ -4,14 +4,14 @@ import java.awt.Image;
 import java.util.ArrayList;
 
 public class Animation {
-	private ArrayList<FrameView> frams;
+	private ArrayList<FrameView> frames;
 	private long trackTime;
 	private long totalTimeShow;
 	private int currentFrame;
 
 	public Animation() {
 
-		this.frams = new ArrayList<>();
+		this.frames = new ArrayList<>();
 		this.trackTime = 0;
 		this.totalTimeShow = 0;
 		this.currentFrame = 0;
@@ -19,11 +19,11 @@ public class Animation {
 
 	public void addFrame(String filename, long duration) {
 		totalTimeShow += duration;
-		frams.add(new FrameView(Loader.loadImage(filename), totalTimeShow));
+		frames.add(new FrameView(Loader.loadImage(filename), totalTimeShow));
 	}
 
 	public FrameView getCurrentFrame() {
-		return frams.get(currentFrame);
+		return frames.get(currentFrame);
 	}
 
 	public Image getCurrentImage() {
@@ -31,7 +31,7 @@ public class Animation {
 	}
 
 	public void update(long elapsetime) {
-		if (frams.size() > 1) {
+		if (frames.size() > 1) {
 			trackTime += elapsetime;
 			if (trackTime >= totalTimeShow) {
 				trackTime -= totalTimeShow;
